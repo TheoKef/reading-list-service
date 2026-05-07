2c9de67718d035f2a8550c7584f9310cea6fb3f1
TheoKef <theo.kefala@gmail.com>
2026-05-07 12:34:06 +0300

    1.2-entities-dtos

---
+++ src/main/java/com/senior/engineer/course/readinglistservice/domain/Book.java
@@ -0,0 +1,28 @@
+package com.senior.engineer.course.readinglistservice.domain;
+
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+  //@Id
+  //@GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  private String author;
+  private String isbn;
+  private Integer publicationYear;
+
+}
---
+++ src/test/java/com/senior/engineer/course/readinglistservice/ReadingListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senior.engineer.course.readinglistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingListServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
